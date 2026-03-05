de1fe6756e0c0048e0dd57de0cc92eac03f9c9ab
harstario <harstario@hotmail.com>
2026-03-05 10:02:25 +0800

    adding new order

--- producer/producer.py
+++ producer/producer.py
@@ -2,8 +2,6 @@ import uuid
 import json
 from confluent_kafka import Producer
 
-
-
 producer_config = {
     "bootstrap.servers": "localhost:9092",
     "enable.idempotence":True,  # Ensures Exactly-Once at producer level
@@ -25,9 +23,9 @@ def delivery_report(err, msg):
 
 order = {
     "order_id": str(uuid.uuid4()),
-    "user": "sunda",
-    "item": "naan",
-    "quantity": 14,
+    "user": "tango",
+    "item": "briyani",
+    "quantity": 3,
 
 }
 
@@ -42,5 +40,3 @@ producer.produce(
 
 
 producer.flush()
-
-
